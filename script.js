0d84967cf36ba496f6bdb7fed53d0af37b918e0e
phillipe henriques <phillipe_ch@hotmail.com>
2022-08-11 00:09:34 -0300

    relógio digital e background funcionando

--- script.js
+++ script.js
@@ -4,29 +4,26 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     
-    let bomdia = document.querySelector("body");
-    let boatarde = document.querySelector("body");
-    let boanoite = document.querySelector("body");
-    let boamadrugada = document.querySelector("body");
+    let body = document.querySelector("body");
 
-    msg.innerHTML = `Agora são ${hora} horas`
+    // msg.innerHTML = `Agora são ${hora} horas`
 
     if (hora >= 0 && hora < 6) {
-            // Bom madrugada!
-        img.src = ''
-        boamadrugada.url = "imagens/madrugada.png";
+            // Boa madrugada!
+        img.src = 'imagens/madrugada.png'
+
     } else if (hora >= 6 && hora < 12) {
             // Bom dia!
-        img.src = ''
-        bomdia.url = "imagens/dia.png";     
+        img.src = 'imagens/dia.png'
+     
     } else if (hora >= 12 && hora < 18) {
             // Bom tarde! 
-        img.src = ''
-        boatarde.url = "imagens/tarde.png";
+        img.src = 'imagens/tarde.png'
+
     } else {
             // Bom noite! 
-        img.src = ''
-        boanoite.url = "imagens/noite.png";           
+        img.src = 'imagens/noite.png'
+                   
     }
 }
 
@@ -48,8 +45,55 @@ img.src = ''
 } else {
     // Bom noite! 
 img.src = ''
-document.body.style.backgroundAttachment = 'black';           
+document.body.style.background-image = 'imagens/noite.png';           
 }
 }
 
 */
+
+setInterval(showTime, 1000);
+        function showTime() {
+            let time = new Date();
+            let hour = time.getHours();
+            let min = time.getMinutes();
+            let sec = time.getSeconds();
+            am_pm = "AM";
+ 
+            if (hour > 12) {
+                hour -= 12;
+                am_pm = "PM";
+            }
+            if (hour == 0) {
+                hr = 12;
+                am_pm = "AM";
+            }
+ 
+            hour = hour < 10 ? "0" + hour : hour;
+            min = min < 10 ? "0" + min : min;
+            sec = sec < 10 ? "0" + sec : sec;
+ 
+            let currentTime = hour + ":"
+                + min + ":" + sec + am_pm;
+ 
+            document.getElementById("clock")
+                .innerHTML = currentTime;
+        }
+ 
+        showTime();
+
+// fim do relógio
+// começo do calendário
+
+//  setInterval(showTime, 1000);
+//         function showTime() {
+//             let time = new Date();
+//             let hour = time.getDate();
+
+//             day = 
+
+//             let currentDate = 
+            
+//             document.getElementById("data")
+//                 .innerHTML = currentDate;
+
+//         }
